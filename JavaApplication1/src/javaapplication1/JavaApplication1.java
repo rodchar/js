34e07a6c7de55ec2abfa8b41313f0a849eabe4f3
roodney <roodney@roodney-VirtualBox>
2012-08-23 21:34:14 -0400

    My first project

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-
+import java.sql.*;
 /**
  *
  * @author roodney
@@ -14,6 +14,24 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            String url = "jdbc:derby://localhost:1527/sample";
+            Connection con = DriverManager.getConnection(url, "app", "app");
+            PreparedStatement pstmt = con.prepareStatement("SELECT * FROM CUSTOMER");
+            ResultSet rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+
+                System.out.println(rs.getString("NAME"));
+            }
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
